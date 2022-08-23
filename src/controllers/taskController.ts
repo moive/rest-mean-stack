fc8befca8e239411ffd9be64c4b1eb67737d0f08
Moises Velasquez <mvelasquez@pictoric.com>
2022-08-23 13:16:10 -0500

    read all tasks

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -7,13 +7,31 @@ const createTask = async (req: Request | any, res: Response) => {
 
 	const newTask = new task({ name, creator: id });
 
-	const t = await newTask.save();
+	await newTask.save();
 
 	res.status(200).json({
 		ok: true,
 		msg: "Task created successfully",
-		task: t,
+		task: newTask,
 	});
 };
 
-export { createTask };
+const allTask = async (req: Request | any, res: Response) => {
+	const id = req.uid;
+
+	try {
+		const tasks = await task.find({ creator: id }).sort({ createdAt: -1 });
+
+		return res.status(200).json({
+			ok: true,
+			tasks,
+		});
+	} catch (error) {
+		return res.status(404).json({
+			ok: false,
+			msg: "Task not found",
+		});
+	}
+};
+
+export { createTask, allTask };
--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createTask } from "../controllers/taskController";
+import { createTask, allTask } from "../controllers/taskController";
 import verifyToken from "../middlewares/verifyToken";
 
 const taskRouter = Router();
 
 taskRouter.post("/create", [verifyToken], createTask);
+taskRouter.get("/all", [verifyToken], allTask);
 
 export default taskRouter;
