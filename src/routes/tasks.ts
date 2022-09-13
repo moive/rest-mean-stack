6b6dd6a25ab4c5357c8baa547fb4fd27faf1e763
Moises Velasquez <mvelasquez@pictoric.com>
2022-09-13 08:56:49 -0500

    swagger task schema

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -11,6 +11,29 @@ import verifyToken from "../middlewares/verifyToken";
 
 const taskRouter = Router();
 
+/**
+ *  @swagger
+ *  components:
+ *    schemas:
+ *      Task:
+ *        type: object
+ *        properties:
+ *          name:
+ *            type: string
+ *            description: This is the name
+ *          creator:
+ *            type: User
+ *            description: This is the ID of the user
+ *          createdAt:
+ *            type: date
+ *            description: This is the date of creation
+ *        required:
+ *          - name
+ *          - creator
+ *        example:
+ *          name: Application name
+ *          creator: 165g16d4fg1df54gert45
+ */
 taskRouter.post(
 	"/create",
 	[check("name", "Name is required").not().isEmpty(), verifyToken],
