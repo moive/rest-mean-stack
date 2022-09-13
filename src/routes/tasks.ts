084fc15ce9183a1fcc5f960eabbf2a2f0cce3725
Moises Velasquez <mvelasquez@pictoric.com>
2022-09-13 09:18:17 -0500

    get request with JWT token

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -34,13 +34,35 @@ const taskRouter = Router();
  *          name: Application name
  *          creator: 165g16d4fg1df54gert45
  */
+
+/**
+ *  @swagger
+ *  /task/all:
+ *    get:
+ *     summary: Get all tasks
+ *     tags: [Task]
+ *     parameters:
+ *      - name: x-auth-token
+ *        in: header
+ *        description: JWT token valid
+ *        true: true
+ *     requestBody:
+ *       required: false
+ *     responses:
+ *       404:
+ *         description: The user has no  tasks available
+ *       200:
+ *        description: List of logged-in user tasks
+ */
+taskRouter.get("/all", [verifyToken], allTask);
+
 taskRouter.post(
 	"/create",
 	[check("name", "Name is required").not().isEmpty(), verifyToken],
 	validationErrors,
 	createTask
 );
-taskRouter.get("/all", [verifyToken], allTask);
+
 taskRouter.put(
 	"/update/:id",
 	[check("name", "Name is required").not().isEmpty(), verifyToken],
